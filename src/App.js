c9b79900a816c5174dad68a801a309ddd5c8d675
Jack Stitt <jlsonline@gmail.com>
2017-03-11 03:15:33 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false,
+    };
+    this.toggleClicked = this.toggleClicked.bind(this);
+  }
+
+  toggleClicked() {
+    const { clicked } = this.state;
+    this.setState({
+      clicked: !clicked,
+    });
+  }
+
+  render() {
+    const { clicked } = this.state;
+    console.log(clicked);
+    return (
+      <div>
+        <div>
+          <input type="submit" value="ClickMe" onClick={this.toggleClicked} />
+          </div>
+          {clicked && (
+            <div>
+              Clicked
+            </div>
+          )}
+        </div>
+    );
+  }
+}
+
+export default App;
